1501c626f52155ccd1b75140e376e59f6fe465a5
Rohan Mathur <rohan.mathur0@gmail.com>
2020-11-21 19:26:44 +0530

    feat(flask integration):boilerplate

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import os
+import pdfplumber
+import glob
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from wtforms import MultipleFileField
 from flask_wtf import FlaskForm
@@ -6,6 +12,12 @@ from flask_uploads import configure_uploads,  UploadSet
 
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
+
+from pdftotext import get_text
+from pdftotext import get_percentages
+from pdftotext import assign_comparison
+
+
 
 app=Flask(__name__)
 
@@ -34,7 +46,49 @@ def upload():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filenames.append(filename)
     
-    return render_template('index.html', filenames=filenames)
+    ml = ml_model()
+    print(ml)
+    return render_template('upload.html', results=ml)
+
+
+
+def ml_model():
+    path = r'uploads' # use your path
+    all_files = glob.glob(path + "/*.pdf")
+
+    corpus = []
+
+    for file in all_files:
+
+        text = get_text(file)
+        corpus.append(text)
+
+    vect = TfidfVectorizer(min_df=1, stop_words="english")
+    tfidf = vect.fit_transform(corpus)
+    pairwise_similarity = tfidf * tfidf.T 
+
+    list1 = pairwise_similarity.toarray()
+
+    list = get_percentages(list1)
+    comparison = assign_comparison(len(list1))
+
+
+    all_files = [x.replace('uploads\\', '') for x in all_files]
+
+    final = []
+    for i in range(0 , len(list)):
+
+        first  = comparison[i][0] - 1
+        second = comparison[i][1] - 1
+
+        if list[i] > 0.7:
+            s = " Your score is " + str(list[i]*100) + " % between " + str(all_files[first]) + " and " + str(all_files[second])
+            final.append(s)
+        #print(" Your score is " , list[i]*100 , "% between " , all_files[first] , "and" , all_files[second] , "pdfs")
+
+
+    return final
+    #return render_template('index.html')
 
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
