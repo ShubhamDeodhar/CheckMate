e976cf357e84f3d74e4326bd37a5933a9400140a
Shubham Deodhar <deodhar2000@gmail.com>
2020-11-21 15:24:50 +0530

    feat(uploads):complate

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
-from flask import Flask, render_template, request, redirect,flash
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from wtforms import MultipleFileField
 from flask_wtf import FlaskForm
-from flask_uploads import configure_uploads,IMAGES,UploadSet
+from flask_uploads import configure_uploads,  UploadSet
 
-import os
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='thisisasecret'
-app.config['UPLOADED_IMAGES_DEST']='uploads/'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
-pdfs=UploadSet('files',IMAGES)
-configure_uploads(app,pdfs)
+app.config['ALLOWED_EXTENSIONS'] = set([ 'pdf'])
 
 
-class NewFileForm(FlaskForm):
-    files = MultipleFileField('files')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-###@app.route('/')
-#def landing():
-   #return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET','POST'])
 def upload():
-    form = NewFileForm()
-    if form.validate_on_submit():
-        files_filenames = []
-        for file in form.files.data:
-            file_filename = secure_filename(file.filename)
-            
-            files_filenames.append(file_filename)
-        print(files_filenames)
-        return render_template('index.html', form=form)
-    return render_template('index.html', form=form)
+    uploaded_files = request.files.getlist("file[]")
+    filenames = []
+    for file in uploaded_files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filenames.append(filename)
+    
+    return render_template('index.html', filenames=filenames)
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+#@app.route('/uploads/<filename>')
+#def uploaded_file(filename):
+    #return send_from_directory(app.config['UPLOAD_FOLDER'],
+                              # filename)
 
 
 
-if (__name__ == "__main__"):
-   app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
